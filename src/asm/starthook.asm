b98fc8da84a29222c2ce933018f4d8d23d66bbaa
epsilonfive <58345302+epsilonfive@users.noreply.github.com>
2021-02-20 20:00:39 -0500

    Add asm files

--- src/asm/starthook.asm
+++ src/asm/starthook.asm
@@ -1,54 +1,96 @@
+public _InstallStartHook
+public _ClearStartHook
+
 include 'include/ez80.inc'
 include 'include/tiformat.inc'
 include 'include/ti84pceg.inc'
 
-include 'include/hook_equates.inc'
-include 'include/capnhook.inc'
-
-flags		equ	$D00080
-
-;basically taken from this thread: https://www.cemetech.net/forum/viewtopic.php?t=12920&highlight=
-
-public _hook_start
+_hook_start_addr:
+    virtual at ti.appData
 _hook_start:
     db $83
-    ld (ti.plotSScreen),a
+    ;ld (ti.plotSScreen),a
     cp ti.kPrgm       ; was Prgms pressed?
     ret nz
-    ld hl,$D007E0
-    ld b,(hl)
-    ld a,ti.kQuit
-    ;cp b
-    ;jr nz,return
-    call RelocBase
-RelocBase:
-    pop ix
-    lea hl,ix-RelocBase+ShellPrgm ;actual program running stuff
-    res   ti.progExecuting,(iy + ti.newDispF)
-    res   ti.cmdExec,(iy + ti.cmdFlags)
+    ;call ti.NewContext0
+    ;check if there's enough memory
+    ld hl,9
+    call ti.EnoughMem
+    jq c,NotEnoughMemory
+    ;delete the entry program
+    ld hl,Prog_Name
+    ld de,ti.OP1
+    ld bc,3     ; type (1) + name (1) + zero (1)
+    ldir
+    call ti.ChkFindSym
+    ret c
+    call ti.DelVar
+    ;create the new program
+    ld hl,Prog_Name
     call ti.Mov9ToOP1
-    call ti.ExecutePrgm
-    ld a,0
-    ld (ti.plotSScreen),a
-    ld a,ti.cxCmd
-    call ti.NewContext0
-    call ti.SetupHome
-return:
-    ld a,(ti.plotSScreen)
-    ; clear the hook
-    ; VYSION will just make a new one
-    ;jq	ti.ClrRawKeyHook
+    ld hl,9 ;the data is 9 bytes long
+    ld a,05h
+    call ti.CreateVar
+    ;write the stuff to it
+    inc de
+    inc de
+    ld hl, Prog_Data
+    ld bc, Prog_Data_End - Prog_Data
+    ldir
+    ld a, ti.kEnter
+    call ti.JForceCmd
     ret
-ShellPrgm:
-    db ti.ProtProgObj,"VYSION", 0
-hook_start_size := $-_hook_start
+NotEnoughMemory:
+    ld hl, String_Mem
+    call ti.PutS
+    ret
+NotInEditor:
+    ld hl, String_Editor
+    call ti.PutS
+    ret
+Prog_Name:
+    db ti.ProgObj, "#", 0
+Prog_Data:
+    db $bb, $6a, ti.tProg, "VYSION"
+Prog_Data_End:
+String_Mem:
+    db "Not enough memory...", 0
+String_Editor:
+    db "Not in editor...", 0
+load code: $-_hook_start from _hook_start
+end virtual
+db code
 
-public _InstallStartHook
+
 _InstallStartHook:
-    ld	hl,_hook_start
-	ld	de,ti.plotSScreen
-	ld	bc,hook_start_size
+    ld iy, ti.flags
+    ld	hl,_hook_start_addr
+	ld	de,ti.appData
+	ld	bc,lengthof code
 	ldir
-	ld	hl,ti.plotSScreen
+	ld	hl,ti.appData
     ;jp ti.plotSScreen
-	jq	ti.SetGetKeyHook
+	call ti.SetGetKeyHook
+    set 0, (iy + ti.hookflags1)
+    set 5, (iy + ti.hookflags1)
+    call	ti.ClrScrn
+	call	ti.HomeUp
+    ld hl, String_Success
+	call	ti.PutS
+.getkey:
+    ;wait for a keypress
+	call	ti.GetCSC
+	or	a,a
+	jr	z,.getkey
+    ret
+String_Success:
+    db "Successfully installed.", 0
+
+_ClearStartHook:
+    ;all of this is probably unnecessary
+    ld iy, ti.flags
+    call ti.ClrGetKeyHook
+    call ti.ClrRawKeyHook
+    res 0, (iy + ti.hookflags1)
+    res 5, (iy + ti.hookflags1)
+    ret
